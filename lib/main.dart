9f8090132df9a09c0b508457ad4c95fa9e15f1c9
aznoranton <84185556+aznoranton@users.noreply.github.com>
2024-04-06 13:45:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -396,3 +396,271 @@ class FirstScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+second 
+  import 'package:flutter/material.dart';
+import 'package:untitled/screens/first.dart';
+import 'package:untitled/screens/third.dart';
+import 'package:untitled/screens/fourth.dart';
+import 'package:untitled/screens/fifth.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'First Screen',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FirstScreen()),
+                );
+              },
+              child: const Text('First Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ThirdScreen()),
+                );
+              },
+              child: const Text('Third Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FourthScreen()),
+                );
+              },
+              child: const Text('Fourth Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FifthScreen()),
+                );
+              },
+              child: const Text('Fifth Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+third import 'package:flutter/material.dart';
+import 'package:untitled/screens/second.dart';
+import 'package:untitled/screens/first.dart';
+import 'package:untitled/screens/fourth.dart';
+import 'package:untitled/screens/fifth.dart';
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'First Screen',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FirstScreen()),
+                );
+              },
+              child: const Text('First Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondScreen()),
+                );
+              },
+              child: const Text('Second Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FourthScreen()),
+                );
+              },
+              child: const Text('Fourth Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FifthScreen()),
+                );
+              },
+              child: const Text('Fifth Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+fourth
+  import 'package:flutter/material.dart';
+import 'package:untitled/screens/second.dart';
+import 'package:untitled/screens/first.dart';
+import 'package:untitled/screens/third.dart';
+import 'package:untitled/screens/fifth.dart';
+
+class FourthScreen extends StatelessWidget {
+  const FourthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'First Screen',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FirstScreen()),
+                );
+              },
+              child: const Text('First Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondScreen()),
+                );
+              },
+              child: const Text('Second Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ThirdScreen()),
+                );
+              },
+              child: const Text('Third Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FifthScreen()),
+                );
+              },
+              child: const Text('Fifth Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+fifrth
+
+  import 'package:flutter/material.dart';
+import 'package:untitled/screens/second.dart';
+import 'package:untitled/screens/first.dart';
+import 'package:untitled/screens/fourth.dart';
+import 'package:untitled/screens/third.dart';
+
+class FifthScreen extends StatelessWidget {
+  const FifthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'First Screen',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FirstScreen()),
+                );
+              },
+              child: const Text('First Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondScreen()),
+                );
+              },
+              child: const Text('Second Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ThirdScreen()),
+                );
+              },
+              child: const Text('Third Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FourthScreen()),
+                );
+              },
+              child: const Text('Fourth Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
