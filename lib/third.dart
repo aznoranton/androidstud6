0af8d7cef6437793c2f583ea2ff6f55277592271
aznoranton <84185556+aznoranton@users.noreply.github.com>
2024-04-06 13:52:55 +0300

    Update third.dart

--- lib/third.dart
+++ lib/third.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/screens/second.dart';
-import 'package:untitled/screens/first.dart';
-import 'package:untitled/screens/fourth.dart';
-import 'package:untitled/screens/fifth.dart';
+import 'package:untitled/second.dart';
+import 'package:untitled/first.dart';
+import 'package:untitled/fourth.dart';
+import 'package:untitled/fifth.dart';
 
 class ThirdScreen extends StatelessWidget {
   const ThirdScreen({Key? key}) : super(key: key);
