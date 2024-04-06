a93854919de7ac36566ee42bce00f668219d023f
aznoranton <84185556+aznoranton@users.noreply.github.com>
2024-04-06 13:53:16 +0300

    Create fourth.dart

---
+++ lib/fourth.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/second.dart';
+import 'package:untitled/first.dart';
+import 'package:untitled/third.dart';
+import 'package:untitled/fifth.dart';
+
+class FourthScreen extends StatelessWidget {
+  const FourthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'First Screen',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FirstScreen()),
+                );
+              },
+              child: const Text('First Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondScreen()),
+                );
+              },
+              child: const Text('Second Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ThirdScreen()),
+                );
+              },
+              child: const Text('Third Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FifthScreen()),
+                );
+              },
+              child: const Text('Fifth Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
